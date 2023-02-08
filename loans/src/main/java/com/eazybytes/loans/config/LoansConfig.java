0e3e5504547a7d0ba9518bf6fd9b9dbf19e9d2e6
CharonT <44173999+CharonT@users.noreply.github.com>
2023-02-08 15:52:41 +0700

    first commit

---
+++ loans/src/main/java/com/eazybytes/loans/config/LoansConfig.java
@@ -0,0 +1,25 @@
+package com.eazybytes.loans.config;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Configuration
+@ConfigurationProperties(prefix = "loans")
+@Getter
+@Setter
+@ToString
+public class LoansConfig {
+	
+	 private String msg ;
+	 private String buildVersion;
+	 private Map<String, String> mailDetails;
+	 private List<String> activeBranches;
+}
